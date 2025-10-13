85bdb9f00ab79dfec74962fad640e27a812a6eaf
alanfzf <alandavidg13@gmail.com>
2025-10-13 17:24:57 -0600

    feat(nvim): revert back to conform

--- config/nvim/plugin/options.lua
+++ config/nvim/plugin/options.lua
@@ -20,7 +20,7 @@ vim.g.clipboard = {
 -- *Global configs*
 --general
 vim.opt.clipboard = "unnamedplus"
-vim.opt.completeopt = { "fuzzy","menu", "menuone", "noinsert", "noselect" }
+vim.opt.completeopt = { "fuzzy", "menu", "menuone", "noinsert", "noselect" }
 vim.opt.laststatus = 3
 vim.opt.mouse = ""
 vim.opt.winborder = "rounded"
