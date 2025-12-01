6e94ac651eaca9e7cca63520968962031fb8be2e
alanfzf <29936954+alanfzf@users.noreply.github.com>
2025-12-01 08:16:54 +0000

    feat(nvim): adjust used plugins

---
+++ config/nvim/after/plugin/arrow.lua
@@ -0,0 +1,11 @@
+-- ARROW setup
+require("arrow").setup({
+  show_icons = true,
+  leader_key = ";",
+  buffer_leader_key = "m",
+  custom_actions = {
+    open = function(target_file_name)
+      vim.cmd("tabnew " .. target_file_name)
+    end,
+  },
+})
