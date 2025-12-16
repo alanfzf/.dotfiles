18b022be107193b082fc4f6599c45952ae94ed3d
alanfzf <29936954+alanfzf@users.noreply.github.com>
2025-12-16 04:31:35 +0000

    fix(nvim): adjust org refile file location

--- config/nvim/after/plugin/org.lua
+++ config/nvim/after/plugin/org.lua
@@ -1,6 +1,6 @@
 require("orgmode").setup({
   org_agenda_files = "~/dropbox/Apps/Metanote/**/*",
-  org_default_notes_file = "~/dropbox/Apps/Metanote/personal/refile.org",
+  org_default_notes_file = "~/dropbox/Apps/Metanote/refile.org",
   ui = {
     input = {
       use_vim_ui = true,
