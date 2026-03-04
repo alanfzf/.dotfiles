ef39ddf531d7583d0791b300b4f7a3087ce631c6
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-03-04 08:43:52 -0600

    feat(nvim): adjust complete behavior

--- config/nvim/plugin/options.lua
+++ config/nvim/plugin/options.lua
@@ -24,7 +24,7 @@ vim.g.clipboard = {
 -- *Global configs*
 --general
 vim.opt.clipboard = "unnamedplus"
-vim.opt.complete = ".,b,o"
+vim.opt.complete = "o^10,.^5,b^5"
 vim.opt.completeopt = { "fuzzy", "menu", "menuone", "noinsert", "noselect", "popup" }
 vim.opt.autocomplete = true
 vim.opt.laststatus = 3
