4823a77a3941f7ad4dc90e55d105a2840232fb54
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-03-05 12:56:21 -0600

    feat(neovim): go back to blink cmp

--- config/nvim/after/plugin/incomplete.lua
+++ config/nvim/after/plugin/incomplete.lua
@@ -1,2 +1,8 @@
+local ok, incomplete = pcall(require, "incomplete")
+
+if not ok then
+  return
+end
+
 vim.o.completefunc = "v:lua.require'incomplete'.completefunc"
-require("incomplete").extend_filetype("php", { "phpdoc" })
+incomplete.extend_filetype("php", { "phpdoc" })
