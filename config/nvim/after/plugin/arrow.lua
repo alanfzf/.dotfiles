d2363ba1afb871b1ed4ebb446118ed6141ed5946
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-03-12 17:24:30 -0600

    feat(nvim): adjust arrow config

--- config/nvim/after/plugin/arrow.lua
+++ config/nvim/after/plugin/arrow.lua
@@ -1,6 +1,7 @@
 require("arrow").setup({
   show_icons = true,
   leader_key = ";",
+  separate_save_and_remove = true,
   buffer_leader_key = "m",
   custom_actions = {
     open = function(target_file_name)
