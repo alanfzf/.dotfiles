8bda75067993b72a23a92b62a995f523499fe413
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-03-26 14:44:05 -0600

    feat(nvim): adjust direnv keymaps

--- config/nvim/after/plugin/direnv.lua
+++ config/nvim/after/plugin/direnv.lua
@@ -1,9 +1,9 @@
 require("direnv").setup({
   autoload_direnv = true,
   keybindings = {
-    allow = "<Leader>da",
-    deny = "<Leader>dd",
-    reload = "<Leader>dr",
-    edit = "<Leader>de",
+    allow = "<Leader>dva",
+    deny = "<Leader>dvd",
+    reload = "<Leader>dvr",
+    edit = "<Leader>dve",
   },
 })
