f29c6fc445cd6f377c6ea4e84824be793ed7a997
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-03-27 05:57:41 +0000

    feat(windows): adjust script

--- scripts/windows/vi.ps1
+++ scripts/windows/vi.ps1
@@ -1,5 +1,6 @@
 # TODO: maybe handle multiple files?
-$filePath = (Resolve-Path -LiteralPath $args[0]).Path
+$pathInput = if ($args.Count -gt 0) { $args[0] } else { "." }
+$filePath = (Resolve-Path -LiteralPath $pathInput).Path
 
 # NOTE: we need to do this with bash becasue other shells may cause issues
 $wslPath = wsl.exe /bin/bash -c "wslpath '$filePath'"
@@ -10,4 +11,4 @@ if ($LASTEXITCODE -ne 0) {
 }
 
 # Launch Neovim inside WSl
-Start-Process "wsl.exe" -ArgumentList "nvim -p '$wslPath'"
+Start-Process -Wait "wsl.exe" -ArgumentList "nvim -p '$wslPath'" -NoNewWindow
