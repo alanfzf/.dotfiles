b3e2abaa32eada361b8238f72af1e097ca3136b6
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-05-14 01:23:34 +0000

    feat(org): add remote export

--- config/nvim/after/plugin/org.lua
+++ config/nvim/after/plugin/org.lua
@@ -1,6 +1,40 @@
 require("orgmode").setup({
   org_agenda_files = "~/dropbox/org/**/*",
   org_default_notes_file = "~/dropbox/org/refile.org",
+  org_custom_exports = {
+    f = {
+      label = "Export to PDF via org-converter",
+      action = function(exporter)
+        local current_file = vim.api.nvim_buf_get_name(0)
+        local target = vim.fs.joinpath(vim.fn.expand("$HOME"), vim.fn.fnamemodify(current_file, ":t:r") .. ".pdf")
+
+        local command = {
+          "curl",
+          "--fail-with-body",
+          "-sS",
+          "-X",
+          "POST",
+          "https://org-converter.fly.dev/convert",
+          "-F",
+          "file=@" .. current_file,
+          "-o",
+          target,
+        }
+        local on_success = function(output)
+          if #output > 0 then
+            vim.api.nvim_echo({ { table.concat(output, "\n") } }, true, {})
+          end
+          vim.api.nvim_echo({ { "Success, saved: " .. target } }, true, {})
+        end
+
+        local on_error = function(err)
+          print("Error!")
+          vim.api.nvim_echo({ { table.concat(err, "\n"), "ErrorMsg" } }, true, {})
+        end
+        return exporter(command, target, on_success, on_error)
+      end,
+    },
+  },
   ui = {
     input = {
       use_vim_ui = true,
