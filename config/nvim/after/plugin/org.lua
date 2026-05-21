926628076e9393e3babf33e1187d61eba342e0b3
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-05-21 15:01:41 -0600

    feat(nvim): adjustmens to orgmode

--- config/nvim/after/plugin/org.lua
+++ config/nvim/after/plugin/org.lua
@@ -1,43 +1,42 @@
+local function export_to_pdf(exporter)
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local target = vim.fs.joinpath(vim.fn.expand("$HOME"), vim.fn.fnamemodify(current_file, ":t:r") .. ".pdf")
+
+  local command = {
+    "curl",
+    "--fail-with-body",
+    "-sS",
+    "-X",
+    "POST",
+    "https://org-converter.fly.dev/convert",
+    "-F",
+    "file=@" .. current_file,
+    "-o",
+    target,
+  }
+  local on_success = function(output)
+    if #output > 0 then
+      vim.api.nvim_echo({ { table.concat(output, "\n") } }, true, {})
+    end
+    vim.api.nvim_echo({ { "Success, saved: " .. target } }, true, {})
+  end
+
+  local on_error = function(err)
+    print("Error!")
+    vim.api.nvim_echo({ { table.concat(err, "\n"), "ErrorMsg" } }, true, {})
+  end
+  return exporter(command, target, on_success, on_error)
+end
+
 require("orgmode").setup({
   org_agenda_files = "~/dropbox/Apps/Metanote/**/*",
   org_default_notes_file = "~/dropbox/Apps/Metanote/refile.org",
+  org_adapt_indentation = false,
+  org_capture_templates = {},
   org_custom_exports = {
     f = {
       label = "Export to PDF via org-converter",
-      action = function(exporter)
-        local current_file = vim.api.nvim_buf_get_name(0)
-        local target = vim.fs.joinpath(vim.fn.expand("$HOME"), vim.fn.fnamemodify(current_file, ":t:r") .. ".pdf")
-
-        local command = {
-          "curl",
-          "--fail-with-body",
-          "-sS",
-          "-X",
-          "POST",
-          "https://org-converter.fly.dev/convert",
-          "-F",
-          "file=@" .. current_file,
-          "-o",
-          target,
-        }
-        local on_success = function(output)
-          if #output > 0 then
-            vim.api.nvim_echo({ { table.concat(output, "\n") } }, true, {})
-          end
-          vim.api.nvim_echo({ { "Success, saved: " .. target } }, true, {})
-        end
-
-        local on_error = function(err)
-          print("Error!")
-          vim.api.nvim_echo({ { table.concat(err, "\n"), "ErrorMsg" } }, true, {})
-        end
-        return exporter(command, target, on_success, on_error)
-      end,
-    },
-  },
-  ui = {
-    input = {
-      use_vim_ui = true,
+      action = export_to_pdf,
     },
   },
 })
