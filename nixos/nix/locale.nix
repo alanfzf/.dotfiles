50aa67982b74e5e047743c2f181be96743ea2eac
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-05-21 15:36:40 -0600

    feat(nix): adjust locale

--- nixos/nix/locale.nix
+++ nixos/nix/locale.nix
@@ -1,6 +1,6 @@
 {
   time.timeZone = "America/Guatemala";
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "es_GT.UTF-8";
@@ -11,6 +11,6 @@
     LC_NUMERIC = "es_GT.UTF-8";
     LC_PAPER = "es_GT.UTF-8";
     LC_TELEPHONE = "es_GT.UTF-8";
-    LC_TIME = "es_GT.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
   };
 }
