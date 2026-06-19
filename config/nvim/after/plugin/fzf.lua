5234ceb76a17a1f5de7131a594f6d2da66694170
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-06-18 22:14:10 -0600

    feat(nvim): adjust fzf

--- config/nvim/after/plugin/fzf.lua
+++ config/nvim/after/plugin/fzf.lua
@@ -37,7 +37,6 @@ fzf.setup({
   zoxide = {
     scope = "tab",
     git_root = true,
-    -- actions = { enter = actions.cd },
   },
   -- git start
   -- i stole this from here: https://github.com/gennaro-tedesco/dotfiles/blob/86cf4de5870a64aa8c5fad2269248db9521a1c2d/nvim/lua/plugins/fzf.lua#L45-L56
@@ -59,39 +58,18 @@ fzf.setup({
         end,
       },
     },
-    branches = {
-      cmd = "git branch --all --color",
-    },
   },
   -- git end
   keymap = {
-    builtin = {
-      -- neovim `:tmap` mappings for the fzf win
-      ["<F1>"] = "toggle-help",
-      ["<F2>"] = "toggle-fullscreen",
-      -- Only valid with the 'builtin' previewer
-      ["<F3>"] = "toggle-preview-wrap",
-      ["<F4>"] = "toggle-preview",
-      -- Rotate preview clockwise/counter-clockwise
-      ["<F5>"] = "toggle-preview-ccw",
-      ["<F6>"] = "toggle-preview-cw",
-      ["<C-d>"] = "preview-page-down",
-      ["<C-u>"] = "preview-page-up",
-    },
     fzf = {
       -- fzf '--bind=' options
-      ["ctrl-a"] = "beginning-of-line",
-      ["ctrl-e"] = "end-of-line",
-      ["ctrl-n"] = "kill-line",
-      ["ctrl-b"] = "half-page-up",
-      ["ctrl-f"] = "half-page-down",
-      -- ["ctrl-n"] = "unix-line-discard",
-      ["alt-a"] = "toggle-all",
-      -- Only valid with fzf previewers (bat/cat/git/etc)
-      ["f3"] = "toggle-preview-wrap",
-      ["f4"] = "toggle-preview",
+      ["ctrl-j"] = "ignore",
+      ["ctrl-k"] = "kill-line",
+
       ["ctrl-d"] = "preview-page-down",
       ["ctrl-u"] = "preview-page-up",
+
+      ["alt-a"] = "toggle-all",
       ["ctrl-x"] = "select-all+accept",
     },
   },
