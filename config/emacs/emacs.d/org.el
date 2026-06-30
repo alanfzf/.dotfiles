bfc3f576555c03b6e70a9fa1c38af08877cf9114
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-06-30 16:36:30 -0600

    feat(emacs): adjust emacs code

--- config/emacs/emacs.d/org.el
+++ config/emacs/emacs.d/org.el
@@ -1,3 +1,6 @@
-(org-babel-do-load-languages
-  'org-babel-load-languages
-  '((sql . t)))
+(setq org-directory (expand-file-name "~/webdav/"))
+(setq org-default-notes-file
+  (expand-file-name "refile.org" org-directory))
+(setq org-agenda-files
+      (directory-files-recursively org-directory "\\.org$"))
+(global-set-key (kbd "C-c a") #'org-agenda)
