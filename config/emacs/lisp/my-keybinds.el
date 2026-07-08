871c05ff15a66f387bc29f2c94cfc711f415d8c4
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-07-08 08:09:21 -0600

    feat(emacs): adjust keybinds

--- config/emacs/lisp/my-keybinds.el
+++ config/emacs/lisp/my-keybinds.el
@@ -1,8 +1,10 @@
 ;; keybinds
 (global-set-key (kbd "C-;") 'comment-line)
 (global-set-key (kbd "C-c t") #'ghostel)
-(global-set-key (kbd "C-c d") (lambda()
+(global-set-key (kbd "C-x C-d") (lambda()
                                 (interactive)
                                 (dired default-directory)
                                 ))
 ;; TODO replace the old weird keybinds for dired
+(global-set-key (kbd "M-p") #'backward-paragraph)
+(global-set-key (kbd "M-n") #'forward-paragraph)
