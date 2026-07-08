230fb7020b76a7f0fe45d488b4cad0d44ad86035
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-07-07 22:12:12 -0600

    feat(nvim): delete dead nvim code

--- config/nvim/after/plugin/incomplete.lua
+++
@@ -1,8 +0,0 @@
-local ok, incomplete = pcall(require, "incomplete")
-
-if not ok then
-  return
-end
-
-vim.o.completefunc = "v:lua.require'incomplete'.completefunc"
-incomplete.extend_filetype("php", { "phpdoc" })
