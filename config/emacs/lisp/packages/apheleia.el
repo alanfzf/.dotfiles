3c2ae58068b04df4224df3feb8d56dc9e5b8b02e
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-07-10 17:35:33 -0600

    feat(emacs): add php-cs-fixer formatter to apheleia

--- config/emacs/lisp/packages/apheleia.el
+++ config/emacs/lisp/packages/apheleia.el
@@ -1,4 +1,16 @@
 (use-package apheleia
-  :init
-  (apheleia-global-mode 1)
-  )
+  :config
+  (setf (alist-get 'php-cs-fixer apheleia-formatters)
+    '("php-cs-fixer"
+       "fix"
+       "--using-cache=no"
+       "--quiet"
+       inplace))
+
+  (setf (alist-get 'php-mode apheleia-mode-alist)
+    'php-cs-fixer)
+
+  (setf (alist-get 'php-ts-mode apheleia-mode-alist)
+    'php-cs-fixer)
+
+  (apheleia-global-mode +1))
