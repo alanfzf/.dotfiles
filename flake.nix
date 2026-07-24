166ac412e49818cdd2258c225925d11eac33d0f7
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-07-23 21:31:37 -0600

    feat(misc): adjust

--- flake.nix
+++ flake.nix
@@ -14,7 +14,6 @@
     # dendritic pattern
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
-
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
