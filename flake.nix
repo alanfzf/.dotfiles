e53b5cd27df93a635a07bf020945d419cd405fac
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-09-01 15:44:06 -0600

    build(nix): bump nix version

--- flake.nix
+++ flake.nix
@@ -6,14 +6,14 @@
     # home manager
     home-manager.url = "github:nix-community/home-manager";
     # darwin
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin";
     # wsl
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     # neovim overlay
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     # dendritic pattern
     flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
+    import-tree.url = "github:denful/import-tree";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
